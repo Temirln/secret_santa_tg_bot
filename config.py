c7342777eded612647ea4c1505166532d810f830
Temirln <t1mb3rmn@gmail.com>
2023-12-18 23:50:15 +0600

    python enterpreter

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 ADMIN_ID = os.environ["ADMIN_ID"]
