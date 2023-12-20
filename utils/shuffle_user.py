12087686848502fedfa298bc35a941e78fdd7a9e
Temirln <t1mb3rmn@gmail.com>
2023-12-21 01:46:10 +0600

    configs

--- utils/shuffle_user.py
+++ utils/shuffle_user.py
@@ -2,12 +2,19 @@ import random
 
 
 def arrange_secret_santa(user_ids):
+    """
+    This Function creates USer id Pairs for Secret Santa
+
+    Parameters : list of ids
+    return : dictionary of pairs id
+
+    """
     # Make a copy of the list and shuffle it
     shuffled_ids = user_ids[:]
     random.shuffle(shuffled_ids)
 
     # Arrange in pairs (A gives to B, B gives to C, ..., last gives to A)
-    pairings = {
+    pairings =  {
         shuffled_ids[i]: shuffled_ids[(i + 1) % len(shuffled_ids)]
         for i in range(len(shuffled_ids))
     }
